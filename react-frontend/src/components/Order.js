bd99e6e3643955f2dee4bd3d806b6f6ab2d12575
Rekha I <Rekha-Inba>
2022-12-16 22:08:55 +0530

    Replaced class with functional component & hooks

--- react-frontend/src/components/Order.js
+++ react-frontend/src/components/Order.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
@@ -10,156 +10,125 @@ import axios from 'axios';
 import './order.css';
 const URL = 'http://localhost:3000/api';
 
-class Order extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      orderList: [],
-      searchTerm: '',
-      orderDescription: '',
-      selected_product_ids: [
+export default function Order(){
+    
+    const [activeOrderId, setActiveOrderId] = useState(0);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [orderDescription, setOrderDescription] = useState('');
+    const [editDescription, setEditDescription] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [orderList, setOrderList] = useState([]);
+    const [selected_product_ids, setSelected_Product_Ids] = useState([
         {
-          id: 1,
-          checked: false,
-        },
-        {
-          id: 2,
-          checked: false,
-        },
-        {
-          id: 3,
-          checked: false,
-        },
-        {
-          id: 4,
-          checked: false,
-        },
-      ],
-      editDescription: '',
-      editId: '',
-      showCreate: false,
-      showEdit: false,
-      showAlert: false
-    };
-  }
+            id: 1,
+            checked: false,
+          },
+          {
+            id: 2,
+            checked: false,
+          },
+          {
+            id: 3,
+            checked: false,
+          },
+          {
+            id: 4,
+            checked: false,
+          }
+    ]);
 
-  openCreateModal() {
-    this.setState({
-      showCreate: true
-    })
-  }
-
-  closeCreateModal() {
-    this.setState({
-      showCreate: false
-    })
-  }
-
-  openEditModal(data) {
-    this.setState({
-      showEdit: true,
-      editId: data.id
-    })
-  }
-
-  closeEditModal() {
-    this.setState({
-      showEdit: false
-    })
-  }
-
-  setShowAlert(showAlert) {
-    this.setState({
-      showAlert
-    })
-  }
-
-  async loadOrders() {
-    const response = await axios.get(`${URL}/order`);
-    this.setState({ orderList: response.data })
-  }
-
-  async componentDidMount() {
-    this.loadOrders();
-  }
-
-  async editOrder() {
-    await axios.put(`${URL}/order/` + this.state.editId, { orderDescription: this.state.editDescription })
-      .then(response => {
-        this.setState({ alertmessage: response.data })
-        this.setShowAlert(true);
-        this.closeEditModal();
-        this.loadOrders();
-      })
-      .catch(error => {
-        this.setState({ alertmessage: error.response.data })
-        this.setShowAlert(true);
-        this.closeEditModal();
-      });
-  }
-
-  async deleteOrder(data) {
-    await axios.delete(`${URL}/order/` + data.id)
-      .then(response => {
-        this.setState({ alertmessage: response.data })
-        this.setShowAlert(true);
-        this.closeEditModal();
-        this.loadOrders();
-      })
-      .catch(error => {
-        this.setState({ alertmessage: error.response.data })
-        this.setShowAlert(true);
-        this.closeEditModal();
-      });
-  }
-
-  onProductSelectionChange(productId, checked) {
-    const selectedIds = [...this.state.selected_product_ids];
-    const product = selectedIds.find(x => x.id == productId)
-    product.checked = checked;
-
-    this.setState({selected_product_ids: selectedIds})
-  }
-
-  async createOrder() {
-    await axios.post(`${URL}/orders`, {
-      orderDescription: this.state.orderDescription,
-      product_ids: this.state.selected_product_ids.filter(x => x.checked).map(x => x.id).join(',')
-    })
-      .then(response => {
-        this.setState({ alertmessage: response.data })
-        this.setShowAlert(true);
-        this.closeCreateModal();
-        this.loadOrders();
-      })
-      .catch(error => {
-        this.setState({ alertmessage: error.response.data })
-        this.setShowAlert(true);
-        this.closeCreateModal();
-      });
-  }
-
-  getFilteredOrderList() {
-    if (!this.state.searchTerm) {
-      return this.state.orderList;
+    const openEditModal = (data) => {
+        setShowEditModal(true);
+        setActiveOrderId(data.id);
     }
 
-    return this.state.orderList.filter(order => {
-      return order.id.toString().includes(this.state.searchTerm) || order.orderDescription.toLowerCase().includes(this.state.searchTerm)
-    })
-  }
+    const loadOrders = async () => {
+      const response = await axios.get(`${URL}/order`);
+      setOrderList(response.data);
+    }
+    
+    useEffect(() => {
+      loadOrders();
+    }, []);
 
-  render() {
+    const editOrder = async () => {
+        await axios.put(`${URL}/order/` + activeOrderId, { orderDescription: editDescription })
+        .then(response => {
+            setAlertMessage(response.data);
+            setShowAlert(true);
+            setShowEditModal(false);
+            loadOrders();
+        })
+        .catch(error => {
+            setAlertMessage(error.response.data);
+            setShowAlert(true);
+            setShowEditModal(false);
+        });
+    } 
+
+    const deleteOrder = async (data) => {
+        await axios.delete(`${URL}/order/` + data.id)
+          .then(response => {
+            setAlertMessage(response.data);
+            setShowAlert(true);
+            setShowEditModal(false);
+            loadOrders();
+          })
+          .catch(error => {
+            setAlertMessage(error.response.data);
+            setShowAlert(true);
+            setShowEditModal(false);
+          });
+    }
+
+    const onProductSelectionChange = (productId, checked) => {
+        const selectedIds = [...selected_product_ids];
+        const product = selectedIds.find(x => x.id == productId)
+        product.checked = checked;
+    
+        setSelected_Product_Ids(selectedIds);
+    }
+
+    const createOrder = async () => {
+        await axios.post(`${URL}/orders`, {
+          orderDescription,
+          product_ids: selected_product_ids.filter(x => x.checked).map(x => x.id).join(',')
+        })
+          .then(response => {
+            setAlertMessage(response.data);
+            setShowAlert(true);
+            setShowCreateModal(false);
+            loadOrders();
+          })
+          .catch(error => {
+            setAlertMessage(error.response.data);
+            setShowAlert(true);
+            setShowCreateModal(false);
+          });
+    }
+
+    const getFilteredOrderList = () => {
+        if (!searchTerm) {
+          return orderList;
+        }
+    
+        return orderList.filter(order => {
+          return order.id.toString().includes(searchTerm) || order.orderDescription.toLowerCase().includes(searchTerm)
+        })
+    }
+
     return (
-      <div className="order-management-wrap">
+        <div className="order-management-wrap">
         <h4>Order Management</h4>
         <div className="container">
           <input
             type="text"
             className="search-wrap"
             placeholder="Search by order ID or order description"
-            onChange={(e) => { this.setState({ searchTerm: e.target.value }) }}
+            onChange={(e) => { setSearchTerm(e.target.value) }}
           />
           <Table striped bordered hover>
             <thead>
@@ -172,17 +141,17 @@ class Order extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.orderList && this.getFilteredOrderList().map(({ id, orderDescription, products_count, createdAt }) => (
+              {orderList && getFilteredOrderList().map(({ id, orderDescription, products_count, createdAt }) => (
                 <tr key={id}>
                   <td>{id}</td>
                   <td>{orderDescription}</td>
                   <td>{products_count}</td>
                   <td>{createdAt}</td>
                   <td>
-                    <Button variant="light" onClick={() => this.openEditModal({ id })}>
+                    <Button variant="light" onClick={() => openEditModal({id})}>
                       <i className="bi bi-pencil-fill"></i>
                     </Button></td>
-                  <td><Button variant="light" onClick={() => this.deleteOrder({ id })}>
+                  <td><Button variant="light" onClick={() => deleteOrder({ id })}>
                     <i className="bi bi-trash3-fill"></i>
                   </Button>
                   </td>
@@ -191,33 +160,33 @@ class Order extends React.Component {
             </tbody>
           </Table>
         </div>
-        <Button variant="secondary" size="lg" onClick={() => this.openCreateModal()}>
+        <Button variant="secondary" size="lg" onClick={() => setShowCreateModal(true)}>
           New Order
         </Button>
 
         {/* New order */}
-        <Modal show={this.state.showCreate} onHide={() => this.closeCreateModal()}>
+        <Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
           <Modal.Header closeButton>
             <Modal.Title>New Order</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <InputGroup className="mb-3">
-              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => this.onProductSelectionChange(1, e.target.checked)} />
+              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => onProductSelectionChange(1, e.target.checked)} />
               <Form.Control className="product-text" aria-label="Text input with checkbox" placeholder="1" />
               <Form.Control aria-label="Text input with checkbox" placeholder="This is HP Laptop" />
             </InputGroup>
             <InputGroup className="mb-3">
-              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => this.onProductSelectionChange(2, e.target.checked)} />
+              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => onProductSelectionChange(2, e.target.checked)} />
               <Form.Control className="product-text" aria-label="Text input with checkbox" placeholder="2" />
               <Form.Control aria-label="Text input with checkbox" placeholder="This is Lenovo Laptop" />
             </InputGroup>
             <InputGroup className="mb-3">
-              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => this.onProductSelectionChange(3, e.target.checked)} />
+              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => onProductSelectionChange(3, e.target.checked)} />
               <Form.Control className="product-text" aria-label="Text input with checkbox" placeholder="3" />
               <Form.Control aria-label="Text input with checkbox" placeholder="This is car" />
             </InputGroup>
             <InputGroup className="mb-3">
-              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => this.onProductSelectionChange(4, e.target.checked)} />
+              <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={(e) => onProductSelectionChange(4, e.target.checked)} />
               <Form.Control className="product-text" aria-label="Text input with checkbox" placeholder="4" />
               <Form.Control aria-label="Text input with checkbox" placeholder="This is bike" />
             </InputGroup>
@@ -225,21 +194,21 @@ class Order extends React.Component {
               type="text"
               className="order-desc"
               placeholder="Enter Order Description"
-              onChange={(e) => { this.setState({ orderDescription: e.target.value }) }}
+              onChange={(e) => { setOrderDescription(e.target.value) }}
             />
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={() => this.closeCreateModal()}>
+            <Button variant="secondary" onClick={() => setShowCreateModal(false)}>
               Cancel
             </Button>
-            <Button variant="primary" onClick={() => this.createOrder(this.state.orderDescription)}>
+            <Button variant="primary" onClick={() => createOrder(orderDescription)}>
               Submit
             </Button>
           </Modal.Footer>
         </Modal>
 
         {/* Edit order */}
-        <Modal show={this.state.showEdit} onHide={() => this.closeEditModal()}>
+        <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
           <Modal.Dialog className="edit-dialog-margin">
             <Modal.Header closeButton>
               <Modal.Title>Edit order</Modal.Title>
@@ -249,14 +218,14 @@ class Order extends React.Component {
                 type="text"
                 className="order-desc"
                 placeholder="Enter Order Description"
-                onChange={(e) => { this.setState({ editDescription: e.target.value }) }}
+                onChange={(e) => { setEditDescription(e.target.value) }}
               />
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary" onClick={() => this.closeEditModal()}>
+              <Button variant="secondary" onClick={() => setShowEditModal(false)}>
                 Cancel
               </Button>
-              <Button variant="primary" onClick={() => this.editOrder(this.state.editDescription)}>
+              <Button variant="primary" onClick={() => editOrder(editDescription)}>
                 Edit
               </Button>
             </Modal.Footer>
@@ -264,20 +233,17 @@ class Order extends React.Component {
         </Modal>
 
        {/* Toast */}
-        {this.state.showAlert && (
+        {showAlert && (
           <ToastContainer className="p-3" position="top-end">
-            <Toast onClose={() => this.setShowAlert(false)}>
+            <Toast onClose={() => setShowAlert(false)}>
               <Toast.Header>
                 <strong className="me-auto">Info</strong>
               </Toast.Header>
-              <Toast.Body>{this.state.alertmessage}</Toast.Body>
+              <Toast.Body>{alertMessage}</Toast.Body>
             </Toast>
           </ToastContainer>
         )}
 
       </div>
-    );
-  }
+    )
 }
-
-export default Order;
